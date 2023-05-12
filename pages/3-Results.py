7d7d02fa9773dfc25ad56da5f70ae3fd43437e77
= <Wanwan@Wanwans-MacBook-Pro-2.local>
2023-05-11 21:43:37 -0700

    Updated good ratings evaluation; commented and reorganized for clarity; change to use new utils modules

--- pages/3-Results.py
+++ pages/3-Results.py
@@ -3,25 +3,28 @@ from rdkit import Chem
 from rdkit.Chem import rdChemReactions
 from rdkit.Chem import Draw
 import numpy as np
-import myformat
-myformat.set_font()
+import st_utils, app_utils
+st_utils.set_font()
 
 st.markdown("# Results")
 
 if "prev_data" not in st.session_state or not "settings_initialized" in st.session_state:
     st.markdown("No molecules evaluated yet.")
 else:
-    data = st.session_state["eval_mol"]
-
-    molids = data.molid.unique()
     mols = []
     smiles = []
     good_molids = []
     legends = []
+
+    # process data
+    data = st.session_state["eval_mol"]
+    molids = data.molid.unique()
     for molid in molids:
         tmp_data = data.loc[ data.molid == molid ]
         ratings = tmp_data.rating_mol.values
-        if "3: interesting" in ratings or "5: good" in ratings or "4" in ratings:
+        if app_utils.rating_scale[-3] in ratings \
+                or app_utils.rating_scale[-2] in ratings \
+                or app_utils.rating_scale[-1] in ratings: # rating 3~5
             smiles.append( tmp_data.iloc[0].smiles )
             mols.append( Chem.MolFromSmiles(smiles[-1]) )
             legends.append( f"{str(molid)}" )
@@ -30,19 +33,23 @@ else:
     data_details = st.session_state["eval_details"]
     molids = data_details.molid.unique()
     for molid in molids:
-        if molid not in good_molids:
+        if molid not in good_molids: #only check if it's not already included
             tmp_data = data_details.loc[ data_details.molid == molid ]
             ratings = tmp_data.rating.values
-            if "3: interesting" in ratings or "5: good" in ratings or "4" in ratings:
+            if app_utils.rating_scale[-3] in ratings \
+                    or app_utils.rating_scale[-2] in ratings \
+                    or app_utils.rating_scale[-1] in ratings: # rating 3~5
                 smiles.append( tmp_data.iloc[0].smiles )
                 mols.append( Chem.MolFromSmiles(smiles[-1]) )
                 legends.append( f"{str(molid)}" )
                 good_molids.append(molid)
     
+    # get combined, unique, molid list
     all_molids = data.molid.unique().tolist()
     all_molids.extend( data_details.molid.unique() )
     all_molids = np.unique(all_molids).tolist()
 
+    # Draw
     svg = Chem.Draw.MolsToGridImage(mols,molsPerRow=3,subImgSize=(250,250),legends=legends, useSVG=True)
 
     st.markdown(f"### `{len(mols)}`/`{len(all_molids)}` saved molecules were rated as `interesting` or `good`:")
@@ -57,6 +64,7 @@ else:
                        file_name = user_file
     )
 
+
 # Diagnostic stuff
 st.markdown("-----")
 st.markdown("optional diagnostics (for debugging)")
@@ -66,6 +74,7 @@ if showlog and "settings_initialized" in st.session_state:
     st.session_state["eval_details"]
     st.session_state["eval_general"]
 
-#show_session_state = st.checkbox("see full session state")
+
+#show_session_state = st.checkbox("see full session state") #for diagnostics
 #if show_session_state:
 #    st.session_state
