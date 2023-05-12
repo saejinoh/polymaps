a1eb5480bd064d6999e7515280712f9e5f2a17b4
= <Wanwan@Wanwans-MacBook-Pro-2.local>
2023-05-11 21:31:43 -0700

    Heavily refactored home page, common app utilities, data storage > 'app_utils' module; runs faster now when switching back to settings page

--- 0-Settings.py
+++ 0-Settings.py
@@ -3,99 +3,14 @@ import pandas as pd
 import os, time
 from rdkit import Chem
 from rdkit.Chem import Descriptors
-import myformat
+import st_utils, app_utils
+import gspread_pdlite as gspdl # database connection
 
-homedir = os.path.dirname(__file__)
-
-st.set_page_config(layout="wide")
 st.session_state["settings_initialized"] = True
-#st.secrets["gcp_service_account"]
+st_utils.set_font(widget=16)
 
 
-myformat.set_font(widget=16)
-
-
-# database connection
-import gspread_pdlite as gspdl
-@st.cache_resource(ttl=600)
-def load_sheet():
-    sheet = gspdl.open_sheet(st.secrets.data.google_key, st.secrets["gcp_service_account"])
-    return sheet
-sheet = load_sheet()
-
-# utilities
-def persist_widget(widget,*args,key=None,val0=None,action=lambda: None,**kwargs):
-    """Persist a widget's state by using a dummy key and a callback to ensure syncing of values.
-
-    Args:
-        widget (_type_): _description_
-        key (_type_): _description_
-        val0 (_type_): _description_
-        action (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    
-    Notes:
-        syntax is minimally invasive:
-        ```
-        persist_widget(widget, *(args as would be usually used), **(kwargs) as would be usually used )
-        ```
-        in addition, must provide the mandatory `key` and `val0` arguments explicitly by name instead of positionally.
-    """
-    if key is None:
-        raise ValueError("must provide a key to persist the widget")
-    if val0 is None:
-        raise ValueError("must provide initial value of widget, via val0 argument")
-
-    tmp_key = key + "_"
-    # set up keys
-    if key not in st.session_state:
-        if tmp_key not in st.session_state:
-            st.session_state[tmp_key] = val0
-            st.session_state[key] = val0
-        else:
-            st.session_state[key] = st.session_state[tmp_key]
-    else:
-        st.session_state[tmp_key] = st.session_state[key]
-
-    # set up callback() and return
-    if widget in [st.button, st.download_button, st.form_submit_button]:
-        # technically shouldn't ever be persisting a button...
-        raise ValueError("Does it really make sense to persist a button value?")
-        #def callback():
-        #    st.session_state[key] = st.session_state[tmp_key]
-        #    action()
-        #return widget(*args,key=tmp_key,on_click=callback,**kwargs)
-    else: #should be `on_change``
-        action = kwargs["on_change"]
-        def callback():
-            st.session_state[key] = st.session_state[tmp_key]
-            action()
-        kwargs["on_change"] = callback
-        return widget(*args,key=tmp_key,**kwargs)
-    
 # ===== Setup =====
-#filename = homedir + "/../data/" + "rxntypes_2023-04-10.csv"
-#data_rxn = pd.read_csv(filename,index_col=False)
-url = gspdl.urlfy(st.secrets.data.data_rxn_key)
-data_rxn = gspdl.url_to_df(url)
-
-
-rxn_types = data_rxn.columns.values
-# TMP: eliminate step reactions
-rxn_types = [x for x in rxn_types if not x.startswith("step") and x != "smiles"]
-rxn_types = rxn_types[:-3] #last two elements should be MW and rxn
-rxn_name_alias = {"simple":"alkene-linear"}
-rxn_name_alias_reverse = {"alkene-linear":"simple"}
-names_to_alias = []
-for ix,x in enumerate(rxn_types):
-    if x in rxn_name_alias: #e.g. simple -> alkene-linear
-        names_to_alias.append( (ix,x) )
-for ix,x in names_to_alias:
-    rxn_types[ix] = rxn_name_alias[x]
-st.session_state["rxn_types"] = rxn_types
-
 if "b_update_data" not in st.session_state:
     st.session_state["b_update_data"] = False
 if "b_update_data_batch" not in st.session_state:
@@ -104,19 +19,6 @@ def set_update_data_flag(flag):
     st.session_state["b_update_data"] = flag
     st.session_state["b_update_data_batch"] = flag
 
-if "max_MW" not in st.session_state:
-    st.session_state["max_MW"] = float(data_rxn.MW.max())
-if "max_numftn" not in st.session_state:
-    st.session_state["max_numftn"] = int(data_rxn.num_ftn.max())
-
-if "eval_details" not in st.session_state:
-    #store mol-level under rxn_name = "general"
-    st.session_state["eval_details"] = pd.DataFrame(columns=["molid","molidx","rxn_name","smiles","userinfo","timestamp","rating"])
-if "eval_mol" not in st.session_state:
-    st.session_state["eval_mol"] = pd.DataFrame(columns=["molid","molidx","smiles","userinfo","timestamp","comments_ftn","comments_mol","rating_mol"])
-if "eval_general" not in st.session_state:
-    st.session_state["eval_general"] = pd.DataFrame(columns=["molid","molidx","smiles","userinfo","timestamp","comments_general"])
-
 
 # ===== ===== BEGIN LAYOUT ===== =====
 st.markdown("# Welcome!")
@@ -124,38 +26,25 @@ st.markdown("- On this page, you can choose some basic filters for the molecules
 st.markdown("- On the `Evaluation` tab (in the sidebar), you will be shown monomers, with one highlighted functional group at a time, and the opportunity to rate the suitability of the functional groups for polymerization. \n  - Rating is on a scale of `1 (bad)` - `3 (interesting)` - `5 (good)`. Default is `0 to skip` judging. \n  - *The first load may take a few minutes!* \n")
 st.markdown("- On the `Results` tab, you can view your top-rated molecules, and optionally download a `.csv` of all molecules that you've rated.")
 
-user_info = persist_widget( st.text_input, "email",
+user_info = st_utils.persist_widget( st.text_input, "email",
                                key = "userinfo", val0="",
                                on_change = lambda: None)
-
-def load_user_session():
-    sheet = gspdl.open_sheet(st.secrets.data.google_key, st.secrets["gcp_service_account"])
-
-    ws = sheet.worksheet(st.secrets.data.name_mol)
-    tmp_df = gspdl.worksheet_to_df(ws)
-    st.session_state["eval_mol"] = tmp_df[ tmp_df.userinfo == st.session_state.userinfo ]
-    
-    ws = sheet.worksheet(st.secrets.data.name_details)
-    tmp_df = gspdl.worksheet_to_df(ws)
-    st.session_state["eval_details"] = tmp_df[ tmp_df.userinfo == st.session_state.userinfo ]
-
 
 if "userinfo" in st.session_state \
     and st.session_state["userinfo"] not in ["",None] \
     and "@" in st.session_state["userinfo"]:
     load_previous = st.button("(Optional): load all previous session results",
-                              on_click=load_user_session)
+                              on_click=app_utils.load_user_session)
 
 # ===== Navigation =====
 #st.markdown("## Navigation & Settings")
 #st.markdown("### On next molecule, show...")
-
-rxn_selection = persist_widget( st.selectbox, "polymerization motif",
+rxn_selection = st_utils.persist_widget( st.selectbox, "polymerization motif",
                                key = "rxn_selection", val0 = "choose for me!",
-                               options = ("choose for me!",*rxn_types),
+                               options = ("choose for me!",*st.session_state["rxn_types"]),
                                on_change = lambda: set_update_data_flag(True))
 
-iteration_selection = persist_widget( st.selectbox,
+iteration_selection = st_utils.persist_widget( st.selectbox,
                                      "molecule presentation mode: (sequential or random)",
                                      options = ("random","sequential"),
                                      key="iteration_selection",
@@ -163,7 +52,7 @@ iteration_selection = persist_widget( st.selectbox,
                                      on_change = lambda: set_update_data_flag(True))
 
 # MW
-slider_MW = persist_widget(st.slider, "MW range",
+slider_MW = st_utils.persist_widget(st.slider, "MW range",
                             min_value = 0., max_value = st.session_state.max_MW,
                             key = "slider_MW",
                             val0 = (0.,250.),
@@ -171,19 +60,17 @@ slider_MW = persist_widget(st.slider, "MW range",
 
 # Simplicity/Complexity
 # (# of polymerizations identified, # functional groups, # subsitutents)
-slider_num_ftn = persist_widget(st.slider,"number of selected (above) potentially polymerizable functional groups. If none selected, defaults to any identified functinoal group.",
+slider_num_ftn_specific = st_utils.persist_widget(st.slider,"number of selected (above) potentially polymerizable functional groups. If none selected, defaults to any identified functinoal group.",
                             min_value = 1, max_value = st.session_state.max_numftn,
                             on_change = lambda: set_update_data_flag(True),
                             key="slider_num_ftn_specific",
                             val0 = (1,3) )
 
-slider_num_ftn = persist_widget(st.slider,"number of (any) potentially polymerizable functional groups",
+slider_num_ftn = st_utils.persist_widget(st.slider,"number of (any) potentially polymerizable functional groups",
                             min_value = 1, max_value = st.session_state.max_numftn,
                             on_change = lambda: set_update_data_flag(True),
                             key="slider_num_ftn",
                             val0 = (1,4) )
-
-
 
 # Bulkiness
 
@@ -199,7 +86,7 @@ def log_general_comment():
     comment_dict = {}
     molid, molidx = st.session_state["data_index"]
     comment_dict["molid"],comment_dict["molidx"] = molid, molidx
-    comment_dict["smiles"] = data_rxn.loc[molid].smiles
+    comment_dict["smiles"] = st.session_state["data_rxn"].loc[molid].smiles
     comment_dict["userinfo"] = st.session_state["userinfo"]
     comment_dict["timestamp"] = pd.Timestamp(time.time(),unit="s")
     comment_dict["comments_general"] = st.session_state["comments_general"]
@@ -210,7 +97,7 @@ def log_general_comment():
     # Save
     st.session_state["eval_general"].to_csv("eval_general.csv",index=False)
 
-    ws    = sheet.worksheet( st.secrets.data.name_general )
+    ws    = st.session_state["backend_sheet"].worksheet( st.secrets.data.name_general )
     gspdl.worksheet_append( ws, pd.Series(comment_dict) )
 
 with st.sidebar:
@@ -223,53 +110,5 @@ with st.sidebar:
             comment_area = st.text_area("General comments?","",key="comments_general")
             submitted = st.form_submit_button("submit",on_click=log_general_comment)
 
-
 # Preload data
-@st.cache_data(ttl=600)
-def load_data():
-    if "base_data" in st.session_state:
-        return st.session_state["base_data"][0], st.session_state["base_data"][0]
-
-    # functional group data
-    #filename = homedir + "/../data/" + "fg_analysis_2023-04-21.csv"
-    #data_df = pd.read_csv(filename,index_col=False)
-    url = gspdl.urlfy(st.secrets.data.fgroups_key)
-    data_df = gspdl.url_to_df(url) #still slow (~30 seconds)
-
-    #sheet = gspdl.open_sheet(st.secrets.data.fgroups_key,st.secrets["gcp_service_account"])
-    #ws = sheet.get_worksheet(0)
-    #data_df = gspdl.worksheet_to_df(ws)
-
-    #def toset(mystr):
-    #   return frozenset( ast.literal_eval(mystr))
-    #data_df["matchidx"] = data_df["matchidx"].map(toset)
-    multi = data_df.set_index(["molid","matchidx","rxn_name"])
-
-    # functional group count data; makes initial filtering easier
-    #filename = homedir + "/../../data/" + "rxntypes_2023-04-21.csv"
-    #data_rxn = pd.read_csv(filename,index_col=False)
-    url = gspdl.urlfy(st.secrets.data.data_rxn_key)
-    data_rxn = gspdl.url_to_df(url)
-
-    # evaluations
-    # evaluate #ftnl groups identified
-    if "num_ftn" not in data_rxn.columns:
-        data_rxn["num_ftn"] = 0.
-
-        num_ftn = multi.reset_index().set_index("molid").groupby(["molid"]).agg("nunique").matchidx
-        data_rxn.loc[multi.index.unique("molid"),"num_ftn"] = num_ftn
-
-    # evaluate MW
-    if "MW" not in data_rxn.columns:
-        data_rxn["MW"] = 0.
-        def get_MW(row):
-            mol = Chem.MolFromSmiles(row.smiles)
-            row.MW = Descriptors.MolWt(mol)
-            return row
-        data_rxn = data_rxn.apply(get_MW,axis=1)
-    
-    return multi,data_rxn
-
-if "base_data" not in st.session_state:
-    multi,data_rxn = load_data()
-    st.session_state["base_data"] = (multi,data_rxn)
+app_utils.first_load()
---
+++ app_utils.py
@@ -0,0 +1,216 @@
+import streamlit as st
+st.set_page_config(layout="wide")
+import numpy as np
+import pandas as pd
+import gspread_pdlite as gspdl
+import os
+
+# ===== Notes
+# Global variables in session_state:
+# < load/initialize once >
+# - backend_sheet:      output data worksheet
+# - data_rxn
+# - rxn_types
+# - base_data: (multi_index data, data_rxn)
+
+# < storage >
+# - eval_general
+# - eval_mol        (can load from backend)
+# - eval_details    (can load from backend)
+
+# < state trackers >
+# - settings_initialized
+# - b_update_data
+# - b_update_data_batch
+
+# < settings (e.g. from widgets) >
+# - userinfo
+# - rxn_selection       (from a slider)
+# - iteration_selection 
+# - slider_MW
+# - slider_num_ftn_specific
+# - slider_num_ftn
+
+# < dynamic >
+
+# ===== Constants
+homedir = os.path.dirname(__file__)
+
+rxn_name_alias = {"simple":"alkene-linear"}
+rxn_name_alias_reverse = {"alkene-linear":"simple"}
+
+rating_scale = ("skip","1: bad","2","3: interesting","4","5: good")
+rating_scale = ("skip (don't answer)","0: N/A","1: impossible","2: bad potential","3: workable potential","4: promising potential","5: almost definitely works")
+rating_scale_index = {entry:ix for ix,entry in enumerate(rating_scale)}
+
+
+# ===== Functions
+
+# ----- I/O
+@st.cache_resource(ttl=600)
+def load_sheet():
+    sheet = gspdl.open_sheet(st.secrets.data.google_key, st.secrets["gcp_service_account"])
+    return sheet
+    
+@st.cache_data(ttl=600)
+def load_data():
+    if "base_data" in st.session_state:
+        return st.session_state["base_data"][0], st.session_state["base_data"][0]
+
+    # functional group data
+    filename = homedir + "/../data/" + "fg_analysis_2023-04-21.csv"
+    data_df = pd.read_csv(filename,index_col=False)
+    #url = gspdl.urlfy(st.secrets.data.fgroups_key)
+    #data_df = gspdl.url_to_df(url) #still slow (~30 seconds)
+
+    #sheet = gspdl.open_sheet(st.secrets.data.fgroups_key,st.secrets["gcp_service_account"])
+    #ws = sheet.get_worksheet(0)
+    #data_df = gspdl.worksheet_to_df(ws)
+
+    #def toset(mystr):
+    #   return frozenset( ast.literal_eval(mystr))
+    #data_df["matchidx"] = data_df["matchidx"].map(toset)
+    multi = data_df.set_index(["molid","matchidx","rxn_name"])
+    #if iterate_by_matchidx:
+    #    multi   = data_df.set_index(["molid","rxn_name","ftn_id","matchid"])
+    #else:
+    #    multi = data_df.set_index(["molid","matchidx","rxn_name"])
+
+
+    # functional group count data; makes initial filtering easier
+    #filename = homedir + "/../../data/" + "rxntypes_2023-04-21.csv"
+    #data_rxn = pd.read_csv(filename,index_col=False)
+    url = gspdl.urlfy(st.secrets.data.data_rxn_key)
+    data_rxn = gspdl.url_to_df(url)
+
+    # evaluations
+    # evaluate #ftnl groups identified
+    if "num_ftn" not in data_rxn.columns:
+        data_rxn["num_ftn"] = 0.
+
+        num_ftn = multi.reset_index().set_index("molid").groupby(["molid"]).agg("nunique").matchidx
+        data_rxn.loc[multi.index.unique("molid"),"num_ftn"] = num_ftn
+
+    # evaluate MW
+    if "MW" not in data_rxn.columns:
+        data_rxn["MW"] = 0.
+        def get_MW(row):
+            mol = Chem.MolFromSmiles(row.smiles)
+            row.MW = Descriptors.MolWt(mol)
+            return row
+        data_rxn = data_rxn.apply(get_MW,axis=1)
+    
+    return multi,data_rxn
+
+def load_user_session():
+    #sheet = gspdl.open_sheet(st.secrets.data.google_key, st.secrets["gcp_service_account"])
+    sheet = st.session_state["backend_sheet"]
+
+    ws = sheet.worksheet(st.secrets.data.name_mol)
+    tmp_df = gspdl.worksheet_to_df(ws)
+    st.session_state["eval_mol"] = tmp_df[ tmp_df.userinfo == st.session_state.userinfo ]
+    
+    ws = sheet.worksheet(st.secrets.data.name_details)
+    tmp_df = gspdl.worksheet_to_df(ws)
+    st.session_state["eval_details"] = tmp_df[ tmp_df.userinfo == st.session_state.userinfo ]
+
+
+# ----- Analysis
+def filter_rxn(data, data_rxn, rxn_name = None):
+    """return molids that are consistent"""
+    # TMP, remove step reactions
+    rxn_names = data.index.get_level_values("rxn_name")
+    step_rxn_names = [x for x in rxn_names if x.startswith("step")]
+
+    # filter by reaction name. TODO: step reactions needs adjustment
+    if rxn_name is None or rxn_name == "choose for me!":
+        # TMP, remove step reactions
+        sub_data = data.iloc[ ~data.index.get_level_values("rxn_name").isin(step_rxn_names) ]
+        sub_data = data
+        inds = sub_data.index.get_level_values("molid").unique().values
+    else:
+        # filter by rxn_name
+        if rxn_name in rxn_name_alias:
+            rxn_name = rxn_name_alias_reverse[rxn_name]
+
+        inds = np.argwhere( (data_rxn[rxn_name]>0).values ).flatten() #alternative way to filter
+        #sub_data = data.query("molid in @inds")
+        rxns_of_interest = [rxn_name]
+        sub_data = data.query("rxn_name in @rxns_of_interest")
+        #sub_data = sub_data[ sub_data.index.get_level_values(rxn_name).isin([rxn_name])] 
+
+    if "prev_data" in st.session_state and "slider_MW" in st.session_state:
+        # filter by MW
+        inds_where_MW_range = data_rxn.loc[ 
+            (data_rxn.MW>= st.session_state.slider_MW[0]) & 
+            (data_rxn.MW <= st.session_state.slider_MW[1]) ].index.values
+
+        sub_data = sub_data.query( "molid in @inds_where_MW_range")
+
+        # filter by number of functional groups
+        inds_where_num_ftn_range = data_rxn.loc[ 
+            (data_rxn.num_ftn>= st.session_state.slider_num_ftn[0]) & 
+            (data_rxn.num_ftn <= st.session_state.slider_num_ftn[1]) ].index.values
+        sub_data = sub_data.query( "molid in @inds_where_num_ftn_range")
+
+    # return
+    return sub_data
+
+
+
+# ===== Preprocessing
+# backend data connection
+if "backend_sheet" not in st.session_state:
+    st.session_state["backend_sheet"] = load_sheet()
+
+
+# user log that will be stored to backend
+if "eval_details" not in st.session_state:
+    #store mol-level under rxn_name = "general"
+    st.session_state["eval_details"] = pd.DataFrame(columns=["molid","molidx","rxn_name","smiles","userinfo","timestamp","rating"])
+if "eval_mol" not in st.session_state:
+    st.session_state["eval_mol"] = pd.DataFrame(columns=["molid","molidx","smiles","userinfo","timestamp","comments_ftn","comments_mol","rating_mol"])
+if "eval_general" not in st.session_state:
+    st.session_state["eval_general"] = pd.DataFrame(columns=["molid","molidx","smiles","userinfo","timestamp","comments_general"])
+
+
+# data_rxn
+if "data_rxn" not in st.session_state:
+    url = gspdl.urlfy(st.secrets.data.data_rxn_key)
+    st.session_state["data_rxn"] = gspdl.url_to_df(url)
+    #filename = homedir + "/../data/" + "rxntypes_2023-04-10.csv"
+    #data_rxn = pd.read_csv(filename,index_col=False)
+    #st.session_state["data_rxn"] = data_rxn
+data_rxn = st.session_state["data_rxn"]
+
+
+# rxn_types
+if "rxn_types" not in st.session_state:
+    rxn_types = data_rxn.columns.values
+    # TMP: eliminate step reactions
+    rxn_types = [x for x in rxn_types if not x.startswith("step") and x != "smiles"]
+    rxn_types = rxn_types[:-3] #last two elements should be MW and rxn
+    names_to_alias = []
+    for ix,x in enumerate(rxn_types):
+        if x in rxn_name_alias: #e.g. simple -> alkene-linear
+            names_to_alias.append( (ix,x) )
+    for ix,x in names_to_alias:
+        rxn_types[ix] = rxn_name_alias[x]
+    st.session_state["rxn_types"] = rxn_types
+rxn_types = st.session_state["rxn_types"]
+
+
+# some data_rxn metrics
+if "max_MW" not in st.session_state:
+    st.session_state["max_MW"] = float(data_rxn.MW.max())
+if "max_numftn" not in st.session_state:
+    st.session_state["max_numftn"] = int(data_rxn.num_ftn.max())
+
+
+# molecule data
+def first_load():
+    if "base_data" not in st.session_state:
+        multi,data_rxn = load_data()
+        st.session_state["base_data"] = (multi,data_rxn)
+
+
